d150a873e0e483eec73ad9dbfb81fcc4faaa86bb
Lahiru_Mudith <lahimudith@gmail.com>
2025-03-27 09:51:00 +0530

    create new projevt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.mindbalancecenter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.mindbalancecenter to javafx.fxml;
+    exports org.example.mindbalancecenter;
+}
