5d9d89c84eb987105fb54011ee7a801f360ac07b
Lahiru_Mudith <lahimudith@gmail.com>
2025-03-27 19:21:58 +0530

    create login page ui

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.example.mindbalancecenter {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens org.example.mindbalancecenter.controller to javafx.fxml;
     opens org.example.mindbalancecenter to javafx.fxml;
     exports org.example.mindbalancecenter;
 }
---
+++ src/main/java/org/example/mindbalancecenter/controller/loginPageViewController.java
@@ -0,0 +1,26 @@
+package org.example.mindbalancecenter.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginPageViewController implements Initializable {
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("HIi");
+    }
+}
